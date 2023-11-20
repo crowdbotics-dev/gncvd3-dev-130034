a3dd835016371bc3327bc1f51df4b945510a7e0d
crowdbotics-dev <engineering@crowdbotics.com>
2023-11-20 11:01:15 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Ghbccb
+
+admin.site.register(Ghbccb)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Ghbccb(models.Model):
+    "Generated Model"
+    cxvxb = models.BigIntegerField()
+    reyeui = models.FloatField()
